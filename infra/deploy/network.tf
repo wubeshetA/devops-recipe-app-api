033f547807d116a0483faadcd91dd506f31fa2bb
Wubeshet Yimam <w.yimam@alustudent.com>
2025-02-11 14:28:24 +0300

    Add vpc endpoints

--- infra/deploy/network.tf
+++ infra/deploy/network.tf
@@ -113,3 +113,98 @@ resource "aws_subnet" "private_b" {
   }
 
 }
+
+
+############################################
+# Private subnets for internal access only #
+############################################
+
+
+resource "aws_security_group" "endpoint_access" {
+  description = "Access to the VPC endpoints"
+  vpc_id      = aws_vpc.main.id
+  name        = "${local.prefix}-endpoint-access"
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.main.cidr_block]
+  }
+}
+
+
+resource "aws_vpc_endpoint" "ecr" {
+  vpc_id              = aws_vpc.main.id
+  service_name        = "com.amazonaws.${data.aws_region.current.name}.ecr.api"
+  vpc_endpoint_type   = "Interface"
+  private_dns_enabled = true
+
+  security_group_ids = [aws_security_group.endpoint_access.id]
+  subnet_ids         = [aws_subnet.private_a.id, aws_subnet.private_b.id]
+
+  tags = {
+    Name = "${local.prefix}-ecr-endpoint"
+  }
+}
+
+
+resource "aws_vpc_endpoint" "dkr" {
+  vpc_id              = aws_vpc.main.id
+  service_name        = "com.amazonaws.${data.aws_region.current.name}.ecr.dkr"
+  vpc_endpoint_type   = "Interface"
+  private_dns_enabled = true
+
+  security_group_ids = [aws_security_group.endpoint_access.id]
+  subnet_ids         = [aws_subnet.private_a.id, aws_subnet.private_b.id]
+
+  tags = {
+    Name = "${local.prefix}-dkr-endpoint"
+  }
+}
+
+
+
+resource "aws_vpc_endpoint" "cloudwatch_logs" {
+  vpc_id              = aws_vpc.main.id
+  service_name        = "com.amazonaws.${data.aws_region.current.name}.logs"
+  vpc_endpoint_type   = "Interface"
+  private_dns_enabled = true
+
+  security_group_ids = [aws_security_group.endpoint_access.id]
+  subnet_ids         = [aws_subnet.private_a.id, aws_subnet.private_b.id]
+
+  tags = {
+    Name = "${local.prefix}-cloudwatch-endpoint"
+  }
+}
+
+
+resource "aws_vpc_endpoint" "ssm" {
+  vpc_id              = aws_vpc.main.id
+  service_name        = "com.amazonaws.${data.aws_region.current.name}.ssmmessages"
+  vpc_endpoint_type   = "Interface"
+  private_dns_enabled = true
+
+  security_group_ids = [aws_security_group.endpoint_access.id]
+  subnet_ids         = [aws_subnet.private_a.id, aws_subnet.private_b.id]
+
+  tags = {
+    Name = "${local.prefix}-ssmmessages-endpoint"
+  }
+}
+
+
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id              = aws_vpc.main.id
+  service_name        = "com.amazonaws.${data.aws_region.current.name}.s3"
+  vpc_endpoint_type   = "Gateway"
+  private_dns_enabled = true
+
+
+  route_table_ids = [aws_vpc.main.default_route_table_id]
+
+  tags = {
+    Name = "${local.prefix}-s3-endpoint"
+  }
+}
